c8e907e15abb6dfe9dc8f88b6ac22f29ed121248
proclx <i.protzai@gmail.com>
2025-10-01 22:56:26 +0300

    Add connection string

--- MoneyRules/MoneyRules.UI/App.xaml.cs
+++ MoneyRules/MoneyRules.UI/App.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using MoneyRules.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace MoneyRules.UI
 {
@@ -9,6 +12,23 @@ namespace MoneyRules.UI
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Зчитуємо рядок підключення з App.config
+            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+
+            // Створюємо екземпляр контексту
+            using var context = new AppDbContext(optionsBuilder.Options);
+
+            // Можна зробити EnsureCreated для тесту
+            context.Database.EnsureCreated();
+        }
+
     }
 
 }
