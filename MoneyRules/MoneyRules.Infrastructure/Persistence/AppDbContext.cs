505de53c41e7b7ba96632f33593acf4f2371c0a8
proclx <i.protzai@gmail.com>
2025-10-01 22:38:08 +0300

    Add DB context

---
+++ MoneyRules/MoneyRules.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyRules.Domain.Entities;
+
+namespace MoneyRules.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Settings> Settings { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Fluent API для зв'язків
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Settings)
+                .WithOne(s => s.User)
+                .HasForeignKey<Settings>(s => s.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany<Transaction>()
+                .WithOne(t => t.User)
+                .HasForeignKey(t => t.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany<Category>()
+                .WithOne(c => c.User)
+                .HasForeignKey(c => c.UserId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
